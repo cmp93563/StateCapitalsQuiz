afe52c4586936a7478e9adc76857ef6284c12c36
jennyngo1925 <jennyngo1925@gmail.com>
2023-11-06 13:14:16 -0500

    score page

--- app/src/main/java/edu/uga/cs/statecapitalsquiz/QuizQuestionPagerAdapter.java
+++ app/src/main/java/edu/uga/cs/statecapitalsquiz/QuizQuestionPagerAdapter.java
@@ -21,6 +21,6 @@ public class QuizQuestionPagerAdapter extends FragmentStateAdapter {
 
     @Override
     public int getItemCount() {
-        return QuizQuestionFragment.getNumberOfQuestions();
+        return QuizQuestionFragment.getNumberOfQuestions() + 1;
     }
 }
